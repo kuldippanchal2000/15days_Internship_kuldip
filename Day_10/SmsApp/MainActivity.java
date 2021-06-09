478b05cbf6d6b50ebe920eb7b049b16de5145dd8
kuldippanchal2000 <70636189+kuldippanchal2000@users.noreply.github.com>
2021-06-09 23:20:11 +0530

    Create MainActivity.java

---
+++ Day_10/SmsApp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.emailapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etMobile,etMsg;
+    Button btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etMobile=findViewById(R.id.etMobile);
+        etMsg=findViewById(R.id.etMsg);
+        btnSend=findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String no=etMobile.getText().toString();
+                String msg=etMsg.getText().toString();
+                SmsManager sms=SmsManager.getDefault();
+                sms.sendTextMessage(no,null,msg,null,null);
+                Toast.makeText(getApplicationContext(),"Message sent successfully",
+                Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
