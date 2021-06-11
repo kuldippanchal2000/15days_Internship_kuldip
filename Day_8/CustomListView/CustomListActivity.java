97fba94d1e98eb90677b63cb20d21346b7edc368
kuldippanchal2000 <70636189+kuldippanchal2000@users.noreply.github.com>
2021-06-12 00:28:30 +0530

    Update and rename CustomListViewActivity.java to CustomListActivity.java

--- Day_8/CustomListView/CustomListActivity.java
+++ Day_8/CustomListView/CustomListActivity.java
@@ -8,25 +8,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class CustomListViewActivity extends AppCompatActivity {
-    ListView lv1;
-int image []={R.drawable.fruits,R.drawable.banana,R.drawable.grapes,R.drawable.kivi,R.drawable.oranges};
-String[]name= {"This is fruits","This is banana","This is grapes","This is kivi","This is oranges"};
+public class CustomListActivity extends AppCompatActivity {
+ListView lv1;
+int[] image=(new int[]{R.drawable.watermelon, R.drawable.banana, R.drawable.grapes, R.drawable.kivi, R.drawable.oranges});
+    String[]name= {"This is Watermelon","This is banana","This is grapes","This is kivi","This is oranges"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_list_view);
+        setContentView(R.layout.activity_cutom_list);
         lv1=findViewById(R.id.lv1);
-        MyAdapter myAdapter=new MyAdapter(image, name, CustomListViewActivity.this);
+
+        MyAdapter myAdapter=new MyAdapter(image,name, CustomListActivity.this);
         lv1.setAdapter(myAdapter);
+
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent=new Intent(CustomListViewActivity.this,ListViewDetailActivity.class);
+                Intent intent=new Intent(CustomListActivity.this,ListViewDetailActivity.class);
                 intent.putExtra("image",image[position]);
-                intent.putExtra("name",name[position]);
+                intent.putExtra("name", name [position]);
                 startActivity(intent);
             }
         });
+        }
     }
-}
